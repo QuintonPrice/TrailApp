04627a6b6fa9371207fc49b89800da06a2d41016
Quinton Price <60953984+QuintonPrice@users.noreply.github.com>
2021-10-31 16:54:12 -0700

    creating template to test firebase with

--- trail-app/src/App.js
+++ trail-app/src/App.js
@@ -1,23 +1,33 @@
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className='app'>
+        <header>
+            <div className='wrapper'>
+              <h1>Fun Food Friends</h1>
+              
+            </div>
+        </header>
+        <div className='container'>
+          <section className='add-item'>
+              <form>
+                <input type="text" name="username" placeholder="What's your name?" />
+                <input type="text" name="currentItem" placeholder="What are you bringing?" />
+                <button>Add Item</button>
+              </form>
+          </section>
+          <section className='display-item'>
+            <div className='wrapper'>
+              <ul>
+              </ul>
+            </div>
+          </section>
+        </div>
+      </div>
+    );
+  }
 }
-
 export default App;
