709bf9d956aa73c74fd61bcb442fa8ebe90bae87
Quinton Price <60953984+QuintonPrice@users.noreply.github.com>
2021-11-01 11:44:07 -0700

    Header update

--- trail-app/src/App.js
+++ trail-app/src/App.js
@@ -62,7 +62,7 @@ class App extends Component {
       <div className='app'>
         <header>
             <div className='wrapper'>
-              <h1>Fun Food Friends</h1>
+              <h1>Firebase Testing</h1>
               
             </div>
         </header>
