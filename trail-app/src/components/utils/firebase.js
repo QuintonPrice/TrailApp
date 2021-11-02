b6edcd07d06f85dee30d1f06e530bab5996ae6ca
Quinton Price <60953984+QuintonPrice@users.noreply.github.com>
2021-11-01 22:59:45 -0700

    .env functionality

--- trail-app/src/components/utils/firebase.js
+++ trail-app/src/components/utils/firebase.js
@@ -4,13 +4,13 @@ import { getDatabase } from 'firebase/database';
 import  { getAuth, GoogleAuthProvider } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBEqnXRP23Dk5FoAqHHhSz3es1pNe2bUA0",
-  authDomain: "trail-app-5b2b7.firebaseapp.com",
-  projectId: "trail-app-5b2b7",
-  storageBucket: "trail-app-5b2b7.appspot.com",
-  messagingSenderId: "571751211472",
-  appId: "1:571751211472:web:dd517f5764930d4d3299d5",
-  measurementId: "G-FJW2JET3YK"
+  apiKey: process.env.REACT_APP_APIKEY,
+  authDomain: process.env.REACT_APP_AUTHDOMAIN,
+  projectId: process.env.REACT_APP_PROJECTID,
+  storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
+  appId: process.env.REACT_APP_APPID,
+  measurementId: process.env.REACT_APP_MEASUREMENTID
 };
 
 // Initialize Firebase
