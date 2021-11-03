9d1ccf4666227621677fa89079a46db754521ff7
Quinton Price <60953984+QuintonPrice@users.noreply.github.com>
2021-11-02 18:37:31 -0700

    Removed app.js.swp

--- trail-app/src/components/NavBar/NavBar.js
+++ trail-app/src/components/NavBar/NavBar.js
@@ -5,8 +5,7 @@ import './navbar.css';
 import { default as NavbarBS } from 'react-bootstrap/Navbar';
 import { default as NavBS } from 'react-bootstrap/Nav';
 
-import { Link } from 'react-router-dom'; // imports link functionality
-import { render } from 'react-dom';
+//import { Link } from 'react-router-dom'; // imports link functionality
 
 // navigation bar class
 class NavBar extends Component {
@@ -14,7 +13,7 @@ class NavBar extends Component {
     render() {
         return (
             <NavbarBS id="navb" className="sticky-top navbar-fixed-top navbar-dark shadow mr-auto" expand="lg" >
-                <li><img className="user-profile-pic" src={this.props.photoURL}></img></li>
+                <li><img className="user-profile-pic" alt="" src={this.props.photoURL}></img></li>
                 <li>
                     {this.props.user ?
                     <button id="login-button" type="button" className="btn btn-md btn-primary" onClick = {() => this.props.logoutFunction()}>Logout</button>
