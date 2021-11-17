d9f230ea75b64a27d4db0affc8221fe205e92b75
Quinton Price <60953984+QuintonPrice@users.noreply.github.com>
2021-11-16 16:44:18 -0800

    Some form validation and success message

--- trail-app/src/components/Cards/Card.js
+++ trail-app/src/components/Cards/Card.js
@@ -8,28 +8,28 @@ class Card extends Component {
         return (
             <div className="card-deck">
                 <div className="card">
-                    <img className="card-img-top" src="https://blog.corp-site.envato.com/cdn-cgi/image/width=1200,quality=85,format=auto/uploads/2020/06/SOC101_Portfolio_Guide.png"/>
+                    <img className="card-img-top" alt="" src="https://s24953.pcdn.co/blog/wp-content/uploads/2018/01/Templates-Guide-header-1-1024x576.png"/>
                     <div className="card-body">
                         <h5 className="card-title">Card Title</h5>
                         <p className="card-text">This is a longer card test. This is a longer card test. This is a longer card test.</p>
                     </div>
                 </div>
                 <div className="card">
-                    <img className="card-img-top" src="https://blog.corp-site.envato.com/cdn-cgi/image/width=1200,quality=85,format=auto/uploads/2020/06/SOC101_Portfolio_Guide.png"/>
+                    <img className="card-img-top" alt="" src="https://s24953.pcdn.co/blog/wp-content/uploads/2018/01/Templates-Guide-header-1-1024x576.png"/>
                     <div className="card-body">
                         <h5 className="card-title">Card Title</h5>
                         <p className="card-text">This is a longer card test. This is a longer card test. This is a longer card test.</p>
                     </div>
                 </div>
                 <div className="card">
-                    <img className="card-img-top" src="https://blog.corp-site.envato.com/cdn-cgi/image/width=1200,quality=85,format=auto/uploads/2020/06/SOC101_Portfolio_Guide.png"/>
+                    <img className="card-img-top" alt="" src="https://s24953.pcdn.co/blog/wp-content/uploads/2018/01/Templates-Guide-header-1-1024x576.png"/>
                     <div className="card-body">
                         <h5 className="card-title">Card Title</h5>
                         <p className="card-text">This is a longer card test. This is a longer card test. This is a longer card test.</p>
                     </div>
                 </div>
                 <div className="card">
-                    <img className="card-img-top" src="https://blog.corp-site.envato.com/cdn-cgi/image/width=1200,quality=85,format=auto/uploads/2020/06/SOC101_Portfolio_Guide.png"/>
+                    <img className="card-img-top" alt="" src="https://s24953.pcdn.co/blog/wp-content/uploads/2018/01/Templates-Guide-header-1-1024x576.png"/>
                     <div className="card-body">
                         <h5 className="card-title">Card Title</h5>
                         <p className="card-text">This is a longer card test. This is a longer card test. This is a longer card test.</p>
--- trail-app/src/pages/Trails/Trails.js
+++ trail-app/src/pages/Trails/Trails.js
@@ -1,9 +1,9 @@
 // Home page
-import React, { Component } from 'react';
+import React, { Component, useEffect } from 'react';
 import './trails.css';
 
 import Card from "../../components/Cards/Card.js";
-import { Link } from 'react-router-dom'; // imports link functionality
+//import { Link } from 'react-router-dom'; // imports link functionality
 import Modal from "react-bootstrap/Modal";
 import Button from 'react-bootstrap/Button';
 
@@ -13,6 +13,7 @@ class Trails extends Component {
         super();
         this.state = {
             show: false,
+            submitted: false
         }
         this.handleClose = this.handleClose.bind(this);
         this.handleShow = this.handleShow.bind(this);
@@ -20,7 +21,8 @@ class Trails extends Component {
 
     handleClose() {
         this.setState({
-            show: false
+            show: false,
+            submitted: false
         });
     }
 
@@ -31,21 +33,30 @@ class Trails extends Component {
     }
 
     render() {
+        const submitted = this.props.submitted;
+        let successMessage;
+        if (submitted) {
+            successMessage = <div id="successMessage" className="alert alert-success" role="alert">Success! Trail added</div>
+        } else {
+            successMessage = <span></span>
+        }
+        
         return (
             <div id="trails-div">
-                
                 <button onClick={this.handleShow} id="add-trail-button" className="btn btn-lg btn-warning shadow">+ Add New Trail</button>
                 <Modal show={this.state.show} onHide={this.handleClose} size="xl">
                     <Modal.Header>
                         <Modal.Title>Add New Trail</Modal.Title>
                     </Modal.Header>
                     <Modal.Body>
+                        {successMessage}
                         <form onSubmit={(e) => this.props.handleSubmit(e)}>
                             <label for="trailNameInput" className="form-label font-weight-bold">Enter trail name:</label>
-                            <input id="trailNameInput" className="trail-input form-control form-control-lg" type="text" name="trailName" placeholder="Name" onChange={(e) => this.props.handleChange(e)} />
+                            <input id="trailNameInput" required className="trail-input form-control form-control-lg" type="text" name="trailName" placeholder="Name" onChange={(e) => this.props.handleChange(e)} />
                             
                             <label for="trailTypeInput">What type of trail is it?</label>
-                            <select id="trailTypeInput" className="text-dark trail-input form-control form-control-sm" name="trailType" onChange={(e) => this.props.handleChange(e)}>
+                            <select id="trailTypeInput" required className="text-dark trail-input form-control form-control-sm" name="trailType" onChange={(e) => this.props.handleChange(e)}>
+                                <option value="" selected disabled hidden>Choose type</option>
                                 <option value="Hiking">Hiking</option>
                                 <option value="Mountain Bike">Mountain Biking</option>
                                 <option value="Trail Running">Trail Running</option>
@@ -54,12 +65,12 @@ class Trails extends Component {
                             </select>
 
                             <label for="trailLocationInput">Where's the trail located?</label>
-                            <input id="trailLocationInput" className="text-dark trail-input form-control form-control-sm" type="text" name="trailLocation" placeholder="Location" onChange={(e) => this.props.handleChange(e)} />
+                            <input id="trailLocationInput" required className="text-dark trail-input form-control form-control-sm" type="text" name="trailLocation" placeholder="Location" onChange={(e) => this.props.handleChange(e)} />
                             
                             <label for="trailDescriptionInput">Finally, give the trail a description:</label>
-                            <textarea id="trailDescriptionInput" className="text-dark trail-input form-control" rows="3" type="text" name="trailDescription" placeholder="Description" onChange={(e) => this.props.handleChange(e)} />
+                            <textarea id="trailDescriptionInput" required className="text-dark trail-input form-control" rows="3" type="text" name="trailDescription" placeholder="Description" onChange={(e) => this.props.handleChange(e)} />
 
-                            <button id="create-trail-button" type="submit" className="btn btn-md btn-outline-warning" >Create New Trail</button>
+                            <button id="create-trail-button" type="submit" onClick={this.handleCreate} className="btn btn-md btn-outline-warning">Create New Trail</button>
                         </form>
                     </Modal.Body>
                     <Modal.Footer>
